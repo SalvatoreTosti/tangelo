c04d5908c36c3843b4ab1caaeafbab858e3efefb
Salvatore Tosti <saltosti@gmail.com>
2015-01-04 10:59:53 -0800

    set up project with lein

---
+++ src/tangelo/core.clj
@@ -0,0 +1,7 @@
+(ns tangelo.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/tangelo/core_test.clj
@@ -0,0 +1,7 @@
+(ns tangelo.core-test
+  (:require [clojure.test :refer :all]
+            [tangelo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
