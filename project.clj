1e18fcce40f5529e164b9bd16c46aeb6a094d695
SalvatoreTosti <saltosti@gmail.com>
2015-01-05 11:45:17 -0800

    updated description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tangelo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A text editor written in Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
