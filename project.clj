2516ba661336c8c7cf95245f99d4737759bd40b5
SalvatoreTosti <saltosti@gmail.com>
2015-01-05 16:29:21 -0800

    updated information to Artistic Liscence

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tangelo "0.1.0-SNAPSHOT"
   :description "A text editor written in Clojure."
   :url "https://github.com/SalvatoreTosti/tangelo"
-  :license {:name "Eclipse Public License"
+  :license {:name "Artistic License 2.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]]
