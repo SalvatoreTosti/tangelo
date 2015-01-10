110a1c18246ce7f40ec5b23542490d7faf3af1b2
SalvatoreTosti <saltosti@gmail.com>
2015-01-10 14:37:29 -0800

    Added basic link jumping
    By selecting the exact area which contains a link head, you can now jump to it's tail.
    This does not have protections currently, so selections must be exact.

--- src/tangelo/backend.clj
+++ src/tangelo/backend.clj
@@ -3,23 +3,6 @@
   (:require
       [seesaw.core :as seesaw]))
 
-
-(defn add-link-head [datab link-head]
-  (assoc datab link-head #{}))
-
-(defn add-link-pair-helper [datab link-head link-tail]
-  (update-in datab [link-head] #(conj % link-tail)))
-
-;link pairs represented as:
-;{[head-start,head-end] #{[tail-start,tail end]}}
-;Note there can be many 'tail' vectors in the set.
-
-(defn add-link-pair [datab link-head link-tail]
-  (if (contains? datab link-head)
-    (add-link-pair-helper datab link-head link-tail)
-    (add-link-pair-helper (assoc datab link-head #{}) link-head link-tail)))
-
-
 (defn build-full-location [directory name ext]
   (let [sep (System/getProperty "file.separator")
         path (str directory sep name ext)]
--- src/tangelo/gui.clj
+++ src/tangelo/gui.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require
    [seesaw.core :as seesaw]
-   [tangelo.backend :as backend]))
+   [tangelo.backend :as backend]
+   [tangelo.hyperEditor :as hyper]))
 
 (defn build-menubar [text-pane link-helper-atom link-db]
   (seesaw/menubar
@@ -23,32 +24,40 @@
                                            (seesaw/text! text-pane (backend/open-text-file "target/test-text.txt"))
                                            (reset! link-db (backend/open-data-file "target/test-text.lslc"))
                                             ))
-                                (seesaw/action
+                                ])
+
+           (seesaw/menu :text "Link"
+                        :items [(seesaw/action
                                  :name "Hyper start"
                                  :handler (fn [e]
                                             (swap! link-helper-atom assoc :start (seesaw/selection text-pane))))
                                 (seesaw/action
                                  :name "Hyper end"
                                  :handler (fn [e]
-                                            ;nil))
                                             (swap! link-helper-atom assoc :end (seesaw/selection text-pane))))
                                 (seesaw/action
                                  :name "Hyper add"
-                                 :handler (fn[e]
-                                            ;nil))
-                                            (swap! link-db #(backend/add-link-pair % (@link-helper-atom :start) (@link-helper-atom :end)))))
+                                 :handler (fn [e]
+                                            (swap! link-db #(hyper/add-link-pair % (@link-helper-atom :start) (@link-helper-atom :end)))))
+                                (seesaw/action
+                                 :name "Jump link"
+                                 :handler (fn [e]
+                                            (let [tail (first (@link-db (seesaw/selection text-pane)))
+                                                  begin (first tail)
+                                                  end (last tail)]
+                                              (seesaw/config! text-pane :caret-position begin)
+                                              (seesaw/selection! text-pane tail))))
                                 (seesaw/action
                                  :name "View hyper links"
                                  :handler (fn [e]
                                             (println @link-db)))
-
-
-                                 ]
-                        )]))
+                                ])
+           ]))
 
 (defn build-content []
   (let [text-pane (seesaw/text
                      :multi-line? true
+                     :wrap-lines? true
                      :editable? true
                      :margin 20  ;margin in pixels
                      :caret-position 0)]
