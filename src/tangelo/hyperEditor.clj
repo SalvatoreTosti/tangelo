2bceb10eba8eb992e9ba81af819d83f09123c2c7
Salvatore Tosti <saltosti@gmail.com>
2015-01-29 13:30:24 -0800

    updated documentation, indicated depricated methods.
    Added documentation text to functions in hyperEditor.clj.
    Also commented-out and moved depreciated functions.
    These can eventually be removed, but for now I left them as a reference.

--- src/tangelo/hyperEditor.clj
+++ src/tangelo/hyperEditor.clj
@@ -10,21 +10,18 @@
       (assoc :text head-text)
       (assoc :tails (assoc {} tail tail-text))))
 
-(defn add-link-pair [datab head tail-node]
-  ;TODO: consider renaming to 'add-link'
-  "Wrapper for inserting new links into a 'database'."
-  (assoc datab head tail-node))
-
 (defn get-head-text [datab head]
   "Returns string associated with a given 'head'."
   (-> (datab head)
       (:text)))
 
 (defn get-tails [datab head]
+  "Returns map which contains 'tails' associated with given 'head'."
   (-> (datab head)
       (:tails)))
 
 (defn insert-new-link [datab link-helper-atom]
+  "Builds requested tail node, then checks if head and tail satisfy requirements to be added."
   (let [tail-info
         (build-link-node
          (@link-helper-atom :head-text)
@@ -37,20 +34,30 @@
      :else (assoc datab head tail-info))))
 
 (defn text-from-selection [string [head,tail]]
+  "Returns text associated with a given 'selection'."
   (subs string head tail))
 
-(defn add-link-head [datab link-head]
+
+
+#_(defn add-link-pair [datab head tail-node]
+  "DEPRICATED. Wrapper for inserting new links into a 'database'."
+  (assoc datab head tail-node))
+
+#_(defn add-link-head [datab link-head]
+  "DEPRICATED."
   (assoc datab link-head #{}))
 
-(defn add-link-pair-helper [datab link-head link-tail]
+#_(defn add-link-pair-helper [datab link-head link-tail]
+  "DEPRICATED."
   (update-in datab [link-head] #(conj % link-tail)))
 
-(defn add-link-pair [datab link-head link-tail]
+#_(defn add-link-pair [datab link-head link-tail]
+  "DEPRICATED."
   (if (contains? datab link-head)
     (add-link-pair-helper datab link-head link-tail)
     (add-link-pair-helper (assoc datab link-head #{}) link-head link-tail)))
 
-(defn add-new-link [datab link-helper-atom])
+#_(defn add-new-link [datab link-helper-atom])
 
 
 #_(defn extract-link-information [datab link-helper-atom]
